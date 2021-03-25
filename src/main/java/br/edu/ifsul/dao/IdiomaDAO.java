e9b0c2a247ade0a874b63505382114cec10ba0b2
marco <marco@DESKTOP-T7BGI9A>
2021-03-25 07:37:16 -0300

    Push Final

---
+++ src/main/java/br/edu/ifsul/dao/IdiomaDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.dao;
+
+import br.edu.ifsul.modelo.Idioma;
+import java.io.Serializable;
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author marco
+ */
+
+@Stateful
+public class IdiomaDAO<TIPO>  extends DAOGenerico<Idioma> implements Serializable {
+    
+    public IdiomaDAO(){
+        super();
+        classePersistente = Idioma.class;
+    }
+
+}
---
+++ src/main/java/br/edu/ifsul/dao/LivroDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.dao;
+
+import br.edu.ifsul.modelo.Livro;
+import java.io.Serializable;
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author marco
+ */
+
+@Stateful
+public class LivroDAO<TIPO>  extends DAOGenerico<Livro> implements Serializable {
+    
+    public LivroDAO(){
+        super();
+        classePersistente = Livro.class;
+    }
+
+}
